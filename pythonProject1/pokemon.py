6042ce5d13c2575d761a1d89ab7bb44372f5937c
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-09-20 15:21:05 +0100

    called api and get response

--- pythonProject1/pokemon.py
+++ pythonProject1/pokemon.py
@@ -1,1 +1,21 @@
 import requests
+import random
+
+
+def api_call(id):
+    url = f'https://pokeapi.co/api/v2/pokemon/{id}/'
+    response = requests.get(url)
+    pokemon = response.json()
+
+    return {
+        'name': pokemon['name'].capitalize(),
+        'id': pokemon['id'],
+        'height': pokemon['height'],
+        'weight': pokemon['weight'],
+        'experience': pokemon['base_experience']
+    }
+
+
+
+
+
